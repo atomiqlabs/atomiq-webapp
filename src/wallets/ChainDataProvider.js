6e15822ec48635a84a881fb38a7e5d89ffe080f3
adambor <adamborteam@gmail.com>
2025-07-18 20:07:50 +0200

    Initial impl of EVM wallets

--- src/wallets/ChainDataProvider.js
+++ src/wallets/ChainDataProvider.js
@@ -5,16 +5,18 @@ import { useStarknetWalletData } from "./chains/useStarknetWalletData";
 import { useLightningWalletData } from "./chains/useLightningWalletData";
 import { ChainDataContext } from "./context/ChainDataContext";
 import { useMemo } from "react";
+import { EVMWalletWrapper, useEVMWalletData } from "./chains/useEVMWalletData";
 function WrappedChainDataProvider(props) {
     const [starknetChain] = useStarknetWalletData();
     const [solanaChain] = useSolanaWalletData();
+    const [citreaChain] = useEVMWalletData();
     const [lightningChain] = useLightningWalletData();
     const [bitcoinChain, bitcoinModal] = useBitcoinWalletData(useMemo(() => {
         return {
-            STARKNET: starknetChain.wallet?.name,
-            SOLANA: solanaChain.wallet?.name
+            STARKNET: starknetChain?.wallet?.name,
+            SOLANA: solanaChain?.wallet?.name
         };
-    }, [starknetChain.wallet, solanaChain.wallet]));
+    }, [starknetChain?.wallet, solanaChain?.wallet]));
     return (_jsxs(ChainDataContext.Provider, { value: useMemo(() => {
             const res = {
                 BITCOIN: bitcoinChain,
@@ -24,9 +26,11 @@ function WrappedChainDataProvider(props) {
                 res.SOLANA = solanaChain;
             if (starknetChain != null)
                 res.STARKNET = starknetChain;
+            if (citreaChain != null)
+                res.CITREA = citreaChain;
             return res;
-        }, [bitcoinChain, lightningChain, solanaChain, starknetChain]), children: [bitcoinModal, props.children] }));
+        }, [bitcoinChain, lightningChain, solanaChain, starknetChain, citreaChain]), children: [bitcoinModal, props.children] }));
 }
 export function ChainDataProvider(props) {
-    return (_jsx(SolanaWalletWrapper, { children: _jsx(WrappedChainDataProvider, { children: props.children }) }));
+    return (_jsx(EVMWalletWrapper, { children: _jsx(SolanaWalletWrapper, { children: _jsx(WrappedChainDataProvider, { children: props.children }) }) }));
 }
