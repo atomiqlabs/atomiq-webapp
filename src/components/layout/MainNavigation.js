3d82475b4c28d49c2e7c819fb32bb7e7bc651406
tibordulovec <tibordulovec@riesenia.com>
2025-07-25 18:03:52 +0200

    Mobile navigation

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,4 +1,5 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import * as React from 'react';
 import classNames from 'classnames';
 import { Navbar, Container, Nav, Badge, NavDropdown } from 'react-bootstrap';
 import Icon from 'react-icons-kit';
@@ -10,19 +11,37 @@ import { repeat } from 'react-icons-kit/fa/repeat';
 import { history } from 'react-icons-kit/fa/history';
 import { angleDown } from 'react-icons-kit/fa/angleDown';
 import { question } from 'react-icons-kit/fa/question';
+import { close } from 'react-icons-kit/fa/close';
 import { useLocation } from 'react-router-dom';
 // TODO Icons
 const navItems = [
     { link: '/', icon: repeat, title: 'Swap' },
-    { link: '/about', icon: history, title: 'Swap history' },
+    { link: '/about', icon: history, title: 'Swap History' },
     { link: '/faq', icon: question, title: 'Explorer' },
-    { link: '/more', icon: question, title: 'Zblunk' },
-    { link: '/more', icon: question, title: 'Haw' },
-    { link: '/more', icon: question, title: 'Mew' },
+    { link: '/more', icon: question, title: 'About' },
+    { link: '/more-daco', icon: question, title: 'FAQs' },
 ];
 export function MainNavigation() {
     const location = useLocation();
-    return (_jsx(Container, { className: "max-width-100", children: _jsxs(Navbar, { collapseOnSelect: true, className: "main-navigation", children: [_jsx(Navbar.Brand, { href: "/", className: "d-flex flex-column", children: _jsxs("div", { className: "d-flex flex-row", style: { fontSize: '1.5rem' }, children: [_jsx("img", { src: "/main_logo.png", className: "main-navigation__logo" }), FEConstants.bitcoinNetwork !== BitcoinNetwork.MAINNET ? (_jsx(Badge, { className: "ms-2 d-flex align-items-center", bg: "danger", children: "DEVNETO" })) : ('')] }) }), _jsx(Navbar.Collapse, { role: "navigation", id: "basic-navbar-nav", children: _jsxs(Nav, { className: 'main-navigation__nav', children: [navItems.slice(0, 3).map((item) => (_jsxs(Nav.Link, { href: item.link, className: classNames('main-navigation__nav__item', {
-                                    'is-active': location.pathname === item.link,
-                                }), children: [_jsx(Icon, { size: 20, icon: item.icon, className: "main-navigation__item__icon" }), _jsx("span", { className: "main-navigation__item__text", children: item.title })] }, item.link))), _jsx(NavDropdown, { className: "main-navigation__more", title: _jsxs("span", { className: "main-navigation__more__label", children: [_jsx("span", { className: "main-navigation__more__text", children: "More" }), _jsx(Icon, { icon: angleDown, size: 20, className: "main-navigation__more__icon" })] }), menuVariant: "dark", children: navItems.slice(3).map((item) => (_jsxs(NavDropdown.Item, { href: item.link, children: [_jsx(Icon, { size: 16, icon: item.icon, className: "me-2 main-navigation__item__icon" }), item.title] }, item.link))) }), _jsx(NFCSwitch, {})] }) }), _jsx("div", { className: "ms-auto", children: _jsx(WalletConnectionsSummary, {}) })] }) }));
+    const [isOpen, setIsOpen] = React.useState(false);
+    const collapseRef = React.useRef(null);
+    React.useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (collapseRef.current &&
+                !collapseRef.current.contains(event.target) &&
+                !event
+                    .composedPath()
+                    .some((el) => el.classList?.contains('navbar-toggler'))) {
+                setIsOpen(false);
+            }
+        };
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+    return (_jsx(Container, { className: "max-width-100", children: _jsx("div", { children: _jsxs(Navbar, { expand: "lg", collapseOnSelect: true, className: "main-navigation", children: [isOpen && _jsx("div", { className: "main-navigation__overlay" }), _jsx(Navbar.Brand, { href: "/", children: _jsxs("div", { className: "d-flex flex-row", style: { fontSize: '1.5rem' }, children: [_jsx("img", { src: "/main_logo.png", className: "main-navigation__logo is-desktop" }), _jsx("img", { src: "/logo192.png", className: "main-navigation__logo is-mobile" }), FEConstants.bitcoinNetwork !== BitcoinNetwork.MAINNET ? (_jsx(Badge, { className: "main-navigation__network ms-2 align-items-center", bg: "danger", children: "DEVNETO" })) : ('')] }) }), _jsx(Navbar.Toggle, { "aria-controls": "basic-navbar-nav", onClick: () => setIsOpen(!isOpen) }), _jsxs("div", { className: "main-navigation__wallet", children: ["TODO Wallet", _jsx(WalletConnectionsSummary, {})] }), _jsx(Navbar.Collapse, { ref: collapseRef, in: isOpen, role: "navigation", id: "basic-navbar-nav", className: classNames('main-navigation__collapse', { show: isOpen }), children: _jsxs(Nav, { className: "main-navigation__nav", children: [_jsxs("div", { className: "main-navigation__nav__mobile-header", children: [_jsx("img", { src: "/main_logo.png", className: "main-navigation__nav__logo", alt: "logo" }), _jsx("div", { className: "main-navigation__nav__close", onClick: () => setIsOpen(false), children: _jsx(Icon, { size: 20, icon: close }) })] }), navItems.map((item, index) => (_jsxs(Nav.Link, { href: item.link, className: classNames('main-navigation__nav__item', {
+                                        'is-active': location.pathname === item.link,
+                                        'is-mobile': index >= 3,
+                                    }), children: [_jsx(Icon, { size: 20, icon: item.icon, className: "main-navigation__nav__item__icon" }), _jsx("span", { className: "main-navigation__nav__item__text", children: item.title })] }, item.link))), _jsx(NavDropdown, { className: "main-navigation__more", title: _jsxs("span", { className: "main-navigation__more__label", children: [_jsx("span", { className: "main-navigation__more__text", children: "More" }), _jsx(Icon, { icon: angleDown, size: 20, className: "main-navigation__more__icon" })] }), menuVariant: "dark", children: navItems.slice(3).map((item) => (_jsxs(NavDropdown.Item, { href: item.link, children: [_jsx(Icon, { size: 16, icon: item.icon, className: "me-2 main-navigation__item__icon" }), item.title] }, item.link))) }), _jsx(NFCSwitch, {})] }) })] }) }) }));
 }
--- src/components/layout/MainNavigation.tsx
+++ src/components/layout/MainNavigation.tsx
@@ -10,103 +10,135 @@ import { repeat } from 'react-icons-kit/fa/repeat';
 import { history } from 'react-icons-kit/fa/history';
 import { angleDown } from 'react-icons-kit/fa/angleDown';
 import { question } from 'react-icons-kit/fa/question';
+import { close } from 'react-icons-kit/fa/close';
 import { useLocation } from 'react-router-dom';
 
 // TODO Icons
 const navItems = [
   { link: '/', icon: repeat, title: 'Swap' },
-  { link: '/about', icon: history, title: 'Swap history' },
+  { link: '/about', icon: history, title: 'Swap History' },
   { link: '/faq', icon: question, title: 'Explorer' },
-  { link: '/more', icon: question, title: 'Zblunk' },
-  { link: '/more', icon: question, title: 'Haw' },
-  { link: '/more', icon: question, title: 'Mew' },
+  { link: '/more', icon: question, title: 'About' },
+  { link: '/more-daco', icon: question, title: 'FAQs' },
 ];
 
 export function MainNavigation() {
   const location = useLocation();
+  const [isOpen, setIsOpen] = React.useState(false);
+  const collapseRef = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        collapseRef.current &&
+        !collapseRef.current.contains(event.target as Node) &&
+        !event
+          .composedPath()
+          .some((el) => (el as HTMLElement).classList?.contains('navbar-toggler'))
+      ) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
     <Container className="max-width-100">
-      <Navbar collapseOnSelect className="main-navigation">
-        {/* LOGO */}
-        <Navbar.Brand href="/" className="d-flex flex-column">
-          <div className="d-flex flex-row" style={{ fontSize: '1.5rem' }}>
-            <img src="/main_logo.png" className="main-navigation__logo" />
-            {/*TODO not sure what is this*/}
-            {FEConstants.bitcoinNetwork !== BitcoinNetwork.MAINNET ? (
-              <Badge className="ms-2 d-flex align-items-center" bg="danger">
-                DEVNETO
-              </Badge>
-            ) : (
-              ''
-            )}
+      <div>
+        <Navbar expand="lg" collapseOnSelect className="main-navigation">
+          {isOpen && <div className="main-navigation__overlay" />}
+          <Navbar.Brand href="/">
+            <div className="d-flex flex-row" style={{ fontSize: '1.5rem' }}>
+              <img src="/main_logo.png" className="main-navigation__logo is-desktop" />
+              <img src="/logo192.png" className="main-navigation__logo is-mobile" />
+              {/*TODO not sure what is this*/}
+              {FEConstants.bitcoinNetwork !== BitcoinNetwork.MAINNET ? (
+                <Badge className="main-navigation__network ms-2 align-items-center" bg="danger">
+                  DEVNETO
+                </Badge>
+              ) : (
+                ''
+              )}
+            </div>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setIsOpen(!isOpen)} />
+          <div className="main-navigation__wallet">
+            TODO Wallet
+            <WalletConnectionsSummary />
           </div>
-        </Navbar.Brand>
+          <Navbar.Collapse
+            ref={collapseRef}
+            in={isOpen}
+            role="navigation"
+            id="basic-navbar-nav"
+            className={classNames('main-navigation__collapse', { show: isOpen })}
+          >
+            <Nav className="main-navigation__nav">
+              <div className="main-navigation__nav__mobile-header">
+                <img src="/main_logo.png" className="main-navigation__nav__logo" alt="logo" />
+                <div className="main-navigation__nav__close" onClick={() => setIsOpen(false)}>
+                  <Icon size={20} icon={close} />
+                </div>
+              </div>
 
-        {/* NAVIGATION ITEMS */}
-        {/*<div className="d-flex flex-column">*/}
-        {/*  <Navbar.Toggle aria-controls="basic-navbar-nav" className="ms-3" />*/}
-        {/*</div>*/}
-
-        <Navbar.Collapse role="navigation" id="basic-navbar-nav">
-          <Nav className={'main-navigation__nav'}>
-            {navItems.slice(0, 3).map((item) => (
-              <Nav.Link
-                key={item.link}
-                href={item.link}
-                className={classNames('main-navigation__nav__item', {
-                  'is-active': location.pathname === item.link,
-                })}
-              >
-                {/*TODO ADD count BADGE*/}
-                <Icon size={20} icon={item.icon} className="main-navigation__item__icon" />
-                <span className="main-navigation__item__text">{item.title}</span>
-              </Nav.Link>
-            ))}
-
-            <NavDropdown
-              className="main-navigation__more"
-              title={
-                <span className="main-navigation__more__label">
-                  <span className="main-navigation__more__text">More</span>
-                  <Icon icon={angleDown} size={20} className="main-navigation__more__icon" />
-                </span>
-              }
-              menuVariant="dark"
-            >
-              {navItems.slice(3).map((item) => (
-                <NavDropdown.Item key={item.link} href={item.link}>
-                  <Icon size={16} icon={item.icon} className="me-2 main-navigation__item__icon" />
-                  {item.title}
-                </NavDropdown.Item>
+              {navItems.map((item, index) => (
+                <Nav.Link
+                  key={item.link}
+                  href={item.link}
+                  className={classNames('main-navigation__nav__item', {
+                    'is-active': location.pathname === item.link,
+                    'is-mobile': index >= 3,
+                  })}
+                >
+                  {/*TODO ADD count BADGE*/}
+                  <Icon size={20} icon={item.icon} className="main-navigation__nav__item__icon" />
+                  <span className="main-navigation__nav__item__text">{item.title}</span>
+                </Nav.Link>
               ))}
-            </NavDropdown>
-            <NFCSwitch />
-          </Nav>
-          {/*TODO not sure what is this*/}
-          {/*<Nav*/}
-          {/*  className="d-none d-lg-flex me-auto text-start"*/}
-          {/*  navbarScroll*/}
-          {/*  style={{ maxHeight: "100px" }}*/}
-          {/*>*/}
-          {/*  {navItems.map((item) => (*/}
-          {/*    <Nav.Link*/}
-          {/*      key={item.link}*/}
-          {/*      href={item.link}*/}
-          {/*      className="d-flex flex-row align-items-center"*/}
-          {/*    >*/}
-          {/*      <Icon icon={item.icon} className="d-flex me-1" />*/}
-          {/*      <span>{item.title}</span>*/}
-          {/*    </Nav.Link>*/}
-          {/*  ))}*/}
-          {/*  <NFCSwitch />*/}
-          {/*</Nav>*/}
-        </Navbar.Collapse>
-        {/* WALLET */}
-        <div className="ms-auto">
-          {/* TODO Wallet*/}
-          <WalletConnectionsSummary />
-        </div>
-      </Navbar>
+
+              <NavDropdown
+                className="main-navigation__more"
+                title={
+                  <span className="main-navigation__more__label">
+                    <span className="main-navigation__more__text">More</span>
+                    <Icon icon={angleDown} size={20} className="main-navigation__more__icon" />
+                  </span>
+                }
+                menuVariant="dark"
+              >
+                {navItems.slice(3).map((item) => (
+                  <NavDropdown.Item key={item.link} href={item.link}>
+                    <Icon size={16} icon={item.icon} className="me-2 main-navigation__item__icon" />
+                    {item.title}
+                  </NavDropdown.Item>
+                ))}
+              </NavDropdown>
+              <NFCSwitch />
+            </Nav>
+            {/*TODO not sure what is this*/}
+            {/*<Nav*/}
+            {/*  className="d-none d-lg-flex me-auto text-start"*/}
+            {/*  navbarScroll*/}
+            {/*  style={{ maxHeight: "100px" }}*/}
+            {/*>*/}
+            {/*  {navItems.map((item) => (*/}
+            {/*    <Nav.Link*/}
+            {/*      key={item.link}*/}
+            {/*      href={item.link}*/}
+            {/*      className="d-flex flex-row align-items-center"*/}
+            {/*    >*/}
+            {/*      <Icon icon={item.icon} className="d-flex me-1" />*/}
+            {/*      <span>{item.title}</span>*/}
+            {/*    </Nav.Link>*/}
+            {/*  ))}*/}
+            {/*  <NFCSwitch />*/}
+            {/*</Nav>*/}
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
     </Container>
   );
 }
