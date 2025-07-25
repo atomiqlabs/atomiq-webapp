60e34963781e4f69b8be99e4e5a38cd1cb455230
tibordulovec <tibordulovec@riesenia.com>
2025-07-25 18:52:35 +0200

    Tak nicht

--- config-overrides.js
+++ config-overrides.js
@@ -3,14 +3,6 @@ const webpack = require('webpack');
 module.exports = function override(config, env) {
   //do stuff with the webpack config...
 
-  // Exclude node_modules from source-map-loader to prevent warning spam
-  const sourceMapLoader = config.module.rules
-    .find((rule) => Array.isArray(rule.oneOf))
-    ?.oneOf.find((r) => r.loader && r.loader.includes('source-map-loader'));
-  if (sourceMapLoader) {
-    sourceMapLoader.exclude = /node_modules/;
-  }
-
   config.resolve.fallback = {
     stream: require.resolve('stream-browserify'),
     // assert: require.resolve('assert-browserify'),
