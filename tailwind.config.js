f792d6dbdc27f5a0028795ee4b2acd523de04219
tibordulovec <tibordulovec@riesenia.com>
2025-07-25 18:55:51 +0200

    Defined colors for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}", "./src/**/*.{scss,css}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './src/**/*.{scss,css}'],
   theme: {
     extend: {
       colors: {
-        pink: "#FF2E8C",
-        pinkSlate: "rgba(255,46,140,0.5)",
+        pink: '#FF2E8C',
+        pinkSlate: 'rgba(255,46,140,0.5)',
+        green: '#40E72D',
+        red: '#FF6C6C',
+        yellow: '#F5F505',
+        blue: '#00A7E1',
       },
     },
   },
