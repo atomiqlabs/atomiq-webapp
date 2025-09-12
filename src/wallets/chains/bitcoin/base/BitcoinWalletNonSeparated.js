39cae0d5c0dfd1a7336a55d49be41f3d3d2f2d2d
adambor <adamborteam@gmail.com>
2025-09-12 02:00:32 +0200

    Use unisat api to leave out UTXOs with ordinals

--- src/wallets/chains/bitcoin/base/BitcoinWalletNonSeparated.js
+++ src/wallets/chains/bitcoin/base/BitcoinWalletNonSeparated.js
@@ -1,16 +1,20 @@
 import { ExtensionBitcoinWallet } from "./ExtensionBitcoinWallet";
-import { filterInscriptionUtxosOnlyConfirmed } from "../utils/InscriptionUtils";
+import { getAddressUtxoSetWithoutTokens } from "../utils/UnisatTokensApi";
 /**
  * Bitcoin wallet with no separation between addresses holding Ordinal assets & BTC
  */
 export class BitcoinWalletNonSeparated extends ExtensionBitcoinWallet {
     async _getUtxoPool(sendingAddress, sendingAddressType) {
         let utxos = await super._getUtxoPool(sendingAddress, sendingAddressType);
-        if (this._isOrdinalsAddress(sendingAddress))
-            utxos = await filterInscriptionUtxosOnlyConfirmed(utxos).catch(err => {
-                console.error(err);
-                return [];
+        if (this._isOrdinalsAddress(sendingAddress) && utxos.length > 0) {
+            const utxosWithoutTokens = await getAddressUtxoSetWithoutTokens(sendingAddress).catch(err => {
+                console.error("BitcoinWalletNonSeparated: _getUtxoPool(): Failed to check UTXOs without tokens, error: ", err);
+                return null;
             });
+            if (utxosWithoutTokens == null)
+                return [];
+            utxos = utxos.filter(val => utxosWithoutTokens.has(val.txId + ":" + val.vout));
+        }
         return utxos;
     }
 }
--- src/wallets/chains/bitcoin/base/BitcoinWalletNonSeparated.ts
+++ src/wallets/chains/bitcoin/base/BitcoinWalletNonSeparated.ts
@@ -1,6 +1,6 @@
 import {ExtensionBitcoinWallet} from "./ExtensionBitcoinWallet";
 import {BitcoinWalletUtxo, CoinselectAddressTypes} from "@atomiqlabs/sdk";
-import {filterInscriptionUtxosOnlyConfirmed} from "../utils/InscriptionUtils";
+import {getAddressUtxoSetWithoutTokens} from "../utils/UnisatTokensApi";
 
 /**
  * Bitcoin wallet with no separation between addresses holding Ordinal assets & BTC
@@ -14,10 +14,14 @@ export abstract class BitcoinWalletNonSeparated extends ExtensionBitcoinWallet {
         sendingAddressType: CoinselectAddressTypes
     ): Promise<BitcoinWalletUtxo[]> {
         let utxos = await super._getUtxoPool(sendingAddress, sendingAddressType);
-        if(this._isOrdinalsAddress(sendingAddress)) utxos = await filterInscriptionUtxosOnlyConfirmed(utxos).catch(err => {
-            console.error(err);
-            return [];
-        });
+        if(this._isOrdinalsAddress(sendingAddress) && utxos.length>0) {
+            const utxosWithoutTokens = await getAddressUtxoSetWithoutTokens(sendingAddress).catch(err => {
+                console.error("BitcoinWalletNonSeparated: _getUtxoPool(): Failed to check UTXOs without tokens, error: ", err);
+                return null;
+            });
+            if(utxosWithoutTokens==null) return [];
+            utxos = utxos.filter(val => utxosWithoutTokens.has(val.txId+":"+val.vout));
+        }
         return utxos;
     }
 
